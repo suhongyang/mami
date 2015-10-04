46816fb069ad84ccda987c320c31a68fdef88b1b
Oly Su <taiya1229@gmail.com>
2015-10-04 21:07:50 +1300

    Setted up child-theme. Added piwik tracking footer.php

---
+++ wp-content/themes/mami/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the main and #page div elements.
+ *
+ * @since 1.0.0
+ */
+$bavotasan_theme_options = bavotasan_theme_options();
+?>
+	</main><!-- main -->
+
+	<footer id="footer" role="contentinfo">
+		<div id="footer-content" class="container">
+			<div class="row">
+				<div class="copyright col-lg-12">
+					<span class="pull-left"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'arcade' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+					<span class="credit-link pull-right"><i class="fa fa-leaf"></i><?php printf( __( 'The %s Theme by %s.', 'arcade' ), BAVOTASAN_THEME_NAME, '<a href="http://themes.bavotasan.com/themes/arcade">bavotasan.com</a>' ); ?></span>
+				</div><!-- .col-lg-12 -->
+			</div><!-- .row -->
+		</div><!-- #footer-content.container -->
+	</footer><!-- #footer -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//taozimama.com/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//taozimama.com/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
+</body>
+</html>
